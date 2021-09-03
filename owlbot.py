876b4357901f92ed84751e9df0c05144c0b08167
Neenu1995 <neenushaji@google.com>
2021-09-03 17:43:42 -0400

    chore: fix replacements

--- owlbot.py
+++ owlbot.py
@@ -48,12 +48,12 @@ for library in s.get_staging_dirs():
         "\g<1>\n\n" + GET_IAM_POLICY
     )
     s.replace(
-        library / f'gapic-google-cloud-{service}-{version}-java/src/**/CloudTasksClient.java',
+        'owl-bot-staging/{version}/gapic-google-cloud-{service}-{version}-java/src/**/CloudTasksClient.java',
         SET_IAM_POLICY_PREVIOUS,
         "\g<1>\n\n" + SET_IAM_POLICY
     )
     s.replace(
-        library / f'gapic-google-cloud-{service}-{version}-java/src/**/CloudTasksClient.java',
+        'owl-bot-staging/{version}/gapic-google-cloud-{service}-{version}-java/src/**/CloudTasksClient.java',
         TEST_IAM_POLICY_PREVIOUS,
         "\g<1>\n\n" + TEST_IAM_POLICY
     )
